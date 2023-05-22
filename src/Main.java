094744c7b571a08cf4baa9a76e39bed4b48835ec
artem <artempolevoj@yandex.ru>
2023-05-22 20:45:51 +0300

    Add class OfferNotebook

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import javax.swing.*;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,10 +6,10 @@ public class Main {
         DataNoteBook dataNoteBook = new DataNoteBook();
         GeneratorNoteBook generatorNoteBook = new GeneratorNoteBook(dataNoteBook, countNotebook);
         Parameter parameter = new Parameter(dataNoteBook);
-        OfferNotebook offerNotebook = new OfferNotebook(parameter,generatorNoteBook);
+        OfferNotebook offerNotebook = new OfferNotebook(parameter, generatorNoteBook);
         ArrayList<NoteBook> userNotebook = offerNotebook.getListNotebook();
         System.out.println("Вывод подходящих ноутбуков:");
-        if (userNotebook.size() > 0){
+        if (userNotebook.size() > 0) {
             userNotebook.forEach(System.out::println);
         } else {
             System.out.println("Нет подходящих ноутбуков.");
